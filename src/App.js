42b031359adafd14b9f2515ed28b14c7cf5cb8ca
Kanav14 <kanavsharma30@gmail.com>
2024-12-02 14:30:44 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import './index.css';
 function App() {
   return (
     <div>
+      {/* Sections */}
+      <div className="section">Section 1</div>
+      <div className="section">Section 2</div>
+      <div className="section">Section 3</div>
+      <div className="section">Section 4</div>
+      <div className="section">Section 5</div>
+      <div className="section">Section 6</div>
+      <div className="section">Section 7</div>
+      <div className="section">Section 8</div>
+      <div className="section">Section 9</div>
+
       {/* Vertical dotted lines */}
-      <div className="dotted-line vertical-line" style={{ left: "33.33%" }}></div>
-      <div className="dotted-line vertical-line" style={{ left: "66.66%" }}></div>
+      <div className="line vertical-line" style={{ left: "33.33%" }}></div>
+      <div className="line vertical-line" style={{ left: "66.66%" }}></div>
 
       {/* Horizontal dotted lines */}
-      <div className="dotted-line horizontal-line" style={{ top: "33.33%" }}></div>
-      <div className="dotted-line horizontal-line" style={{ top: "66.66%" }}></div>
+      <div className="line horizontal-line" style={{ top: "33.33%" }}></div>
+      <div className="line horizontal-line" style={{ top: "66.66%" }}></div>
     </div>
   );
 }
