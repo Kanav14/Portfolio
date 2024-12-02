e15aa6138ce786d06d5c103d551613d235a3103a
Kanav14 <kanavsharma30@gmail.com>
2024-12-02 15:31:13 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import './index.css';
+import './App.css';
 
 function App() {
   return (
-    <div className="grid-container">
-      <div className="grid-item">Section 1</div>
-      <div className="grid-item">Section 2</div>
-      <div className="grid-item">Section 3</div>
-      <div className="grid-item">Section 4</div>
-      <div className="grid-item">Section 5</div>
-      <div className="grid-item">Section 6</div>
-      <div className="grid-item">Section 7</div>
-      <div className="grid-item">Section 8</div>
-      <div className="grid-item">Section 9</div>
-      {/* Vertical and horizontal lines */}
-      <div className="dashed-vertical"></div>
-      <div className="dashed-vertical"></div>
-      <div className="dashed-horizontal"></div>
-      <div className="dashed-horizontal"></div>
+    <div className="app-container">
+      {/* Vertical lines */}
+      <div className="line vertical-line-1"></div>
+      <div className="line vertical-line-2"></div>
+
+      {/* Horizontal lines */}
+      <div className="line horizontal-line-1"></div>
+      <div className="line horizontal-line-2"></div>
     </div>
   );
 }
