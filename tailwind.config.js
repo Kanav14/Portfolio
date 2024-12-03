68ff7bf585474ab3f1b5815c3d413cd47908ecb9
Kanav14 <kanavsharma30@gmail.com>
2024-12-03 14:37:12 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-// tailwind.config.js
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -6,18 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      animation: {
-        "glow-bounce": "glow-bounce 1.2s ease-in-out infinite",
-      },
-      keyframes: {
-        "glow-bounce": {
-          "0%, 100%": { backgroundPosition: "0% 0%" },
-          "50%": { backgroundPosition: "200% 0%" },
-        },
-      },
       colors: {
-        glow: {
-          DEFAULT: "#0066FF", // Darker blue glow color
+        blue: {
+          500: "#0066FF", // Updated glow color
         },
       },
     },
