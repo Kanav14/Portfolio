cfcfc8529c9a430eccaedc53426b8ac1bc7f10c7
Kanav14 <kanavsharma30@gmail.com>
2024-12-03 21:03:58 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,24 @@
 // tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
       animation: {
-        "line-dash": "dash-flow 2s infinite linear alternate",
-        typewriter: "typing 3s steps(20, end), blink-caret 0.5s step-end infinite",
+        "glow-flow": "glow-animation 2s infinite",
       },
       keyframes: {
-        "dash-flow": {
-          "0%": { backgroundPosition: "0 0" },
-          "100%": { backgroundPosition: "40px 0" },
-        },
-        typing: {
-          from: { width: "0" },
-          to: { width: "15ch" },
-        },
-        "blink-caret": {
-          from: { borderColor: "transparent" },
-          to: { borderColor: "white" },
+        "glow-animation": {
+          "0%": { backgroundPosition: "200% 0%" },
+          "100%": { backgroundPosition: "0% 0%" },
         },
       },
       colors: {
         glow: {
-          DEFAULT: "#0044cc", // Dark glossy blue
+          DEFAULT: "#00FFFF", // Cyan glow color
         },
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
       },
     },
   },
