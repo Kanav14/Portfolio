8eb152f35c9ecbdc9815b440e71c996e0942eb78
Kanav14 <kanavsharma30@gmail.com>
2024-12-08 01:16:07 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,14 +46,16 @@ function App() {
             </motion.h1>
           </div>
 
-          {/* Right part: White background with the fixed GIF */}
+          {/* Right part: White background with Loader */}
           <div className="flex-1 bg-[#efefef] flex justify-center items-center relative">
-            <img
-              src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZG1sbzZyM3FjbTF5ZXpmMXlscG9oMnQ3bWVycDBkZnY3amEwOHI1aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WtTnAfZn6aVJfBzlN3/giphy.gif"
-              alt="Cloud with rain"
-              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 h-auto max-w-full"
-              style={{ pointerEvents: "none" }}
-            />
+            {/* Loader */}
+            <div className="loader-container">
+              <div className="loader">
+                {[...Array(12)].map((_, i) => (
+                  <div key={i} className="circle"></div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       ) : (
